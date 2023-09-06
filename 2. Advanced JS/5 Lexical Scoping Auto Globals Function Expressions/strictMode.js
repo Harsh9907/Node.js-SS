3841e6f48b1b311da85ad8e5fe07ef39bbfb103d
Harsh9907 <harshdeep0029@gmail.com>
2023-09-06 19:43:06 +0530

    Adding lexical scoping

---
+++ 2. Advanced JS/5 Lexical Scoping Auto Globals Function Expressions/strictMode.js
@@ -0,0 +1,17 @@
+// "use strict";
+
+// function fun(){
+//     var studentName = "Harsh";
+//     content = "JS";
+//     console.log(studentName);
+// }
+// fun();
+// console.log(content);
+
+console.log("hi");
+console.lo("hello");
+console.log("hello");
+
+// console is an object, inside console object log is a function
+// when parsing is done, console is already present in global scope, and no other formal declaration are present so everything went fine.
+// in execution, "lo" is not a function which is present in console. so it throws error. 
