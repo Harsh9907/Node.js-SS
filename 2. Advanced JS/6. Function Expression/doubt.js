2ce4b5030d5eeeb3ddb54f54329ccb676a87ba66
Harsh9907 <harshdeep0029@gmail.com>
2023-09-07 19:50:20 +0530

    Adding doubt class js file

---
+++ 2. Advanced JS/6. Function Expression/doubt.js
@@ -0,0 +1,21 @@
+let x;
+x=10;
+console.log(x);
+x= NaN;
+
+console.log(x != x )
+
+function fn(){
+    console.log("Hi Harsh!")
+}
+fn();
+
+let y = {
+    "A" : "123",
+    "B" : "456"
+};
+
+console.log(JSON.stringify(y));
+
+//prototype pollution
+// web security in javascript and safe practices followed in javascript
