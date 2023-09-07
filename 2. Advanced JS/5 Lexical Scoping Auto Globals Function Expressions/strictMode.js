f2d051c070a60d0b9e6288b069c0326301e7c33b
Harsh9907 <harshdeep0029@gmail.com>
2023-09-07 12:14:35 +0530

    strict mode usage in block scope and global scope

--- 2. Advanced JS/5 Lexical Scoping Auto Globals Function Expressions/strictMode.js
+++ 2. Advanced JS/5 Lexical Scoping Auto Globals Function Expressions/strictMode.js
@@ -2,16 +2,25 @@
 
 // function fun(){
 //     var studentName = "Harsh";
-//     content = "JS";
+//     content = "JS"; // no auto globalization of this variable content due to strict mode usage.
 //     console.log(studentName);
 // }
 // fun();
 // console.log(content);
 
-console.log("hi");
-console.lo("hello");
-console.log("hello");
+// console.log("hi");
+// console.lo("hello");
+// console.log("hello");
 
 // console is an object, inside console object log is a function
 // when parsing is done, console is already present in global scope, and no other formal declaration are present so everything went fine.
 // in execution, "lo" is not a function which is present in console. so it throws error. 
+
+// strict mode for function in block scope or global scope 
+
+{
+    function fun(){
+        return "123";
+    }
+}
+console.log(fun()); // Here when we are using the strict mode, the function fun will not get the global scope because it is written in the block scope, but when we are not using the strict mode, here the function fun will get the global scope it will not follow the block scope due to sloppy(non-strict) mode
---
+++ 2. Advanced JS/6. Function Expression/functionExpression.js
@@ -0,0 +1,3 @@
+console.log( (10).toString());
+let num = 29.7
+console.log(Math.floor(num));
