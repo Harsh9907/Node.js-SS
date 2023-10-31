c5b3d40e1edc43729ea2cd0a768bf5dfee5894fd
Harsh9907 <harshdeep0029@gmail.com>
2023-10-31 22:57:54 +0530

    Adding more examples in JS File

--- 2. Advanced JS/4 Is JS Compiled, Lexical Scoping/functionScope.js
+++ 2. Advanced JS/4 Is JS Compiled, Lexical Scoping/functionScope.js
@@ -1,21 +1,21 @@
-// // if(false){
-//     var x = 10;
-// }
-// console.log(x);
-// function fun(){
-//     console.log("Inside function");
-//     console.log(y);
-//     var y = 10;
-// }
+if (true /*false*/) {
+  var x = 10;
+}
+console.log(x);
+function fun() {
+  console.log("Inside function");
+  console.log(y);
+  var y = 10;
+}
 
-// fun();
+fun();
 // console.log(y);
 
 // // var creates global scope and function scope
 // // let creates block scope, and it is accesible only in below written code.
 
 {
-    var harsh = 10;
+  var harsh = 10;
 }
 harsh = 2;
 console.log(harsh);
