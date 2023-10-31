efead8a1f56c666f559e36909f23aac6462de4ac
Harsh9907 <harshdeep0029@gmail.com>
2023-10-31 22:43:36 +0530

    Adding output to coercion file

--- 2. Advanced JS/2 Coercion ToNumber ToString ToBoolean/coercion.js
+++ 2. Advanced JS/2 Coercion ToNumber ToString ToBoolean/coercion.js
@@ -1,17 +1,17 @@
-
 // Abstract and Strict Equality Operators
-console.log(undefined == false);
-console.log(1+2);
-console.log("123" == 123)
-console.log(NaN === NaN)
-console.log("harsh" === "harsh")
-console.log(0 === -0)
-console.log(typeof null)
-console.log(null === null)
+console.log(undefined == false); // false
+console.log(1 + 2); // 3
+console.log("123" == 123); // true
+console.log(NaN === NaN); // false
+console.log("harsh" === "harsh"); // true
+console.log(0 === -0); // true
+console.log(typeof null); // object
+console.log(null === null); // true
+console.log("undefined === null : " + (undefined === null)); // false
 
-let obj1 = {x:10};
+let obj1 = { x: 10 };
 let obj2 = obj1;
-let obj3={x:10};
+let obj3 = { x: 10 };
 
-console.log(obj1 === obj2)
-console.log(obj1 === obj3) //same values but object have different memory locations
+console.log(obj1 === obj2); // true
+console.log(obj1 === obj3); //same values but object have different memory locations // false
