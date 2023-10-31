c7214a35b50a3c4f9ef2f809bfc963e16bd3e9fc
Harsh9907 <harshdeep0029@gmail.com>
2023-10-31 22:53:14 +0530

    Adding more examples in JS File

--- 2. Advanced JS/3 Abstract and Strict Equality, NaN and Special Types/boxing.js
+++ 2. Advanced JS/3 Abstract and Strict Equality, NaN and Special Types/boxing.js
@@ -1,4 +1,5 @@
 console.log((1).toString());
+console.log(typeof (1).toString()); // string
 
 // ==========
 
@@ -7,4 +8,4 @@ console.log(typeof x); // number
 console.log(typeof x.toString()); //string
 console.log(x.toString()); // automatic boxing done by JS
 console.log((15).toString()); // manually boxing done by us
-console.log(1..toString()); // double dot does not give any error, because JS got to know that maybe 1 is float as 1.0, but second dot represents that we are converting this in string.
+console.log((1).toString()); // 1..toString (VS code is not allowing this, it is putting parenthesis automatically) double dot does not give any error, because JS got to know that maybe 1 is float as 1.0, but second dot represents that we are converting this in string.
